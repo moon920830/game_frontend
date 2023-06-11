7ad72dc1f491a969f3828f461d32617d31b838f1
moon920830 <lucasdavis0830@gmail.com>
2023-06-10 17:55:04 -0700

    first commit

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,57 @@
 import React, {
   useEffect,
   useRef,
+  useState,
 } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import Dialog from '@mui/material/Dialog';
 import Grid from '@mui/material/Grid';
+import {
+  createTheme,
+  ThemeProvider,
+} from '@mui/material/styles';
 
 function GameItem({ imageSrc, overlayText }) {
-  const [open, setOpen] = React.useState(false);
+  const theme = createTheme({
+    components: {
+      MuiDialog: {
+        styleOverrides: {
+          paper: {
+            borderRadius: '45px',
+            overflow: 'hidden',
+          },
+        },
+      },
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            paddingRight: '0 !important',
+          },
+        },
+      },
+    },
+  });
+
+  const dialogImageStyle = {
+    width: '1000px',
+    height: '100%',
+    margin: '-20px',
+    marginBottom: '-30px',
+  };
+
+  const overlayBoxRef = useRef(null);
+
+  const handleImageLoad = (e) => {
+    const img = e.target;
+    const box = img.parentNode && img.parentNode.querySelector('.overlay-box');
+    if (box) {
+      box.style.width = `${img.offsetWidth}px`;
+    }
+  };
+
+  const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -18,15 +60,6 @@ function GameItem({ imageSrc, overlayText }) {
   const handleClose = () => {
     setOpen(false);
   };
-  const overlayBoxRef = useRef(null);
-
-  const handleImageLoad = (e) => {
-    const img = e.target;
-    const box = img.parentNode && img.parentNode.querySelector(".overlay-box");
-    if (box) {
-      box.style.width = `${img.offsetWidth}px`;
-    }
-  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -36,82 +69,105 @@ function GameItem({ imageSrc, overlayText }) {
       }
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
     handleResize();
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <Grid
         item
         xs={12}
         sm={6}
         xl={3}
-        style={{ cursor: "pointer", position: "relative" }}
+        style={{ cursor: 'pointer', position: 'relative' }}
         onClick={handleClickOpen}
       >
         <Link to="#">
-        <img
-          src={imageSrc}
-          style={{ maxWidth: "100%" }}
-          alt="game1"
-          onLoad={handleImageLoad}
-        />
-        <Grid
-          container
-          alignItems="center"
-          className="overlay-box"
-          style={{
-            position: "absolute",
-            backgroundColor: "rgba(0, 0, 0, 0.16)",
-            bottom: "5px",
-            borderBottomRightRadius: "10px",
-            borderBottomLeftRadius: "10px",
-            width: "100%", // Added width property
-          }}
-          height="50px"
-          px={2}
-          columnGap={1}
-          ref={overlayBoxRef}
-        >
-          <img src="images/svg5.svg" alt="svg6" />
-          <span style={{ color: "white" }}>{overlayText}</span>
-        </Grid>
+          <img
+            src={imageSrc}
+            style={{ maxWidth: '100%' }}
+            alt="game1"
+            onLoad={handleImageLoad}
+          />
+          <Grid
+            container
+            alignItems="center"
+            className="overlay-box"
+            style={{
+              position: 'absolute',
+              backgroundColor: 'rgba(0, 0, 0, 0.16)',
+              bottom: '5px',
+              borderBottomRightRadius: '10px',
+              borderBottomLeftRadius: '10px',
+              width: '100%', // Added width property
+            }}
+            height="50px"
+            px={2}
+            columnGap={1}
+            ref={overlayBoxRef}
+          >
+            <img src="images/svg5.svg" alt="svg6" />
+            <span style={{ color: 'white' }}>{overlayText}</span>
+          </Grid>
         </Link>
       </Grid>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        maxWidth={false}
-        PaperProps={{
-          style: {
-            borderRadius: "45px",
-            overflow: 'hidden'
-          },
-        }}
-      >
-        <img
-          src={imageSrc}
-          style={{ width: "1000px", height: "100%", margin: '-20px', marginBottom: '-30px' }}
-          alt="game1"
-        />
+      <Dialog open={open} onClose={handleClose} maxWidth={false}>
+        <img src={imageSrc} style={dialogImageStyle} alt="game1" />
       </Dialog>
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
 function GameItem1({ imageSrc, overlayText }) {
+  const theme = createTheme({
+    components: {
+      MuiDialog: {
+        styleOverrides: {
+          paper: {
+            borderRadius: '45px',
+            overflow: 'hidden',
+          },
+        },
+      },
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            paddingRight: '0 !important',
+          },
+        },
+      },
+    },
+  });
+
+  const dialogImageStyle = {
+    width: '1000px',
+    height: '100%',
+    margin: '-20px',
+    marginBottom: '-30px',
+  };
+
   const overlayBoxRef = useRef(null);
 
   const handleImageLoad = (e) => {
     const img = e.target;
-    const box = img.parentNode && img.parentNode.querySelector(".overlay-box");
+    const box = img.parentNode && img.parentNode.querySelector('.overlay-box');
     if (box) {
       box.style.width = `${img.offsetWidth}px`;
     }
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   useEffect(() => {
@@ -122,50 +178,56 @@ function GameItem1({ imageSrc, overlayText }) {
       }
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
     handleResize();
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <Grid
-      item
-      xs={12}
-      sm={6}
-      style={{ cursor: "pointer", position: "relative" }}
-    >
-      <Link to="/blackjack">
-        <img
-          src={imageSrc}
-          style={{ maxWidth: "100%" }}
-          alt="game1"
-          onLoad={handleImageLoad}
-        />
-        <Grid
-          container
-          alignItems="center"
-          className="overlay-box"
-          style={{
-            position: "absolute",
-            backgroundColor: "rgba(0, 0, 0, 0.16)",
-            bottom: "5px",
-            borderBottomRightRadius: "10px",
-            borderBottomLeftRadius: "10px",
-            width: "100%", // Added width property
-          }}
-          height="50px"
-          px={2}
-          columnGap={1}
-          ref={overlayBoxRef}
-        >
-          <img src="images/svg5.svg" alt="svg6" />
-          <span style={{ color: "white" }}>{overlayText}</span>
-        </Grid>
-      </Link>
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        style={{ cursor: 'pointer', position: 'relative' }}
+        onClick={handleClickOpen}
+      >
+        <Link to="#">
+          <img
+            src={imageSrc}
+            style={{ maxWidth: '100%' }}
+            alt="game1"
+            onLoad={handleImageLoad}
+          />
+          <Grid
+            container
+            alignItems="center"
+            className="overlay-box"
+            style={{
+              position: 'absolute',
+              backgroundColor: 'rgba(0, 0, 0, 0.16)',
+              bottom: '5px',
+              borderBottomRightRadius: '10px',
+              borderBottomLeftRadius: '10px',
+              width: '100%', // Added width property
+            }}
+            height="50px"
+            px={2}
+            columnGap={1}
+            ref={overlayBoxRef}
+          >
+            <img src="images/svg5.svg" alt="svg6" />
+            <span style={{ color: 'white' }}>{overlayText}</span>
+          </Grid>
+        </Link>
+      </Grid>
+      <Dialog open={open} onClose={handleClose} maxWidth={false}>
+        <img src={imageSrc} style={dialogImageStyle} alt="game1" />
+      </Dialog>
+    </ThemeProvider>
   );
 }
 
@@ -178,41 +240,71 @@ function Content() {
         height="100%"
         direction="row"
         style={{
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          color: "white",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          color: 'white',
         }}
       >
         <Grid
           container
           maxWidth="90%"
           spacing={1}
-          style={{ margin: "auto", textAlign: "start" }}
+          style={{ margin: 'auto', textAlign: 'start' }}
         >
           <Grid item xs={12}>
             Howl Originals
           </Grid>
           <Grid item container direction="row" spacing={2} xs={12} mt={1}>
-            <GameItem imageSrc="images/game1.png" overlayText="BlackJack" />
-            <GameItem imageSrc="images/game2.png" overlayText="Coinflip" />
-            <GameItem imageSrc="images/game3.png" overlayText="Wheel" />
-            <GameItem imageSrc="images/game4.png" overlayText="Jackpot" />
+            <GameItem
+              imageSrc="images/game1.png"
+              overlayText="BlackJack"
+            />
+            <GameItem
+              imageSrc="images/game2.png"
+              overlayText="Coinflip"
+            />
+            <GameItem
+              imageSrc="images/game3.png"
+              overlayText="Wheel"
+            />
+            <GameItem
+              imageSrc="images/game4.png"
+              overlayText="Jackpot"
+            />
           </Grid>
-          <Grid item xs={12} mt={3}>
-            Crowl
-          </Grid>
-          <Grid item container direction="row" spacing={2} xs={12} mt={1}>
-            <GameItem1 imageSrc="images/game5.png" overlayText="BlackJack" />
-            <GameItem1 imageSrc="images/game6.png" overlayText="Coinflip" />
-          </Grid>
-          <Grid item xs={12} mt={3}>
+          <Grid item xs={12} mt={6}>
             Howl Originals
           </Grid>
           <Grid item container direction="row" spacing={2} xs={12} mt={1}>
-            <GameItem imageSrc="images/game1.png" overlayText="BlackJack" />
-            <GameItem imageSrc="images/game2.png" overlayText="Coinflip" />
-            <GameItem imageSrc="images/game3.png" overlayText="Wheel" />
-            <GameItem imageSrc="images/game4.png" overlayText="Jackpot" />
+            <GameItem1
+              imageSrc="images/game5.png"
+              overlayText="BlackJack"
+            />
+            <GameItem1
+              imageSrc="images/game6.png"
+              overlayText="Coinflip"
+            />
+          </Grid>
+          <Grid item xs={12} mt={6}>
+            Howl Originals
+          </Grid>
+          <Grid item container direction="row" spacing={2} xs={12} mt={1}>
+            <GameItem
+              imageSrc="images/game1.png"
+              overlayText="BlackJack"
+            />
+            <GameItem
+              imageSrc="images/game2.png"
+              overlayText="Coinflip"
+            />
+            <GameItem
+              imageSrc="images/game3.png"
+              overlayText="Wheel"
+            />
+            <GameItem
+              imageSrc="images/game4.png"
+              overlayText="Jackpot"
+            />
           </Grid>
         </Grid>
       </Grid>
